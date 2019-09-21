3c71b415ae3762f240ab9491d1316b416f9ff0d7
Sivam Patel <44931160+sivampatel@users.noreply.github.com>
2019-09-20 20:34:06 -0500

    update 1

--- hw2.asm
+++ hw2.asm
@@ -6,8 +6,7 @@ wordOutput: .asciiz " word(s) "
 prompt:	.asciiz "Enter some text"
 end:	.asciiz "Goodbye"
 dialogType: .asciiz "Program has ended. "
-string: .space 50
-empyty: .asciiz ""
+string: .space 100
 
 .text 
 main:	li $v0, 54
@@ -16,42 +15,40 @@ main:	li $v0, 54
 	li $a2, 50
 	syscall
 	
-	jal count
+	bne $a1, 0, exit # check for new / successful user input
+	
+	jal count # count number of words and characters in string
 	
 	sw $v0, words
 	sw $v1, chars
 	
 	j print
 	
-	
-count:	#a0 = address of string
-	la $t5, string # store address of string
+count:	#t0 = address of string, $t1 = current byte (character), v0 = words counter, v1 = characters counter
+	la $t0, string # store address of string
 	li $v0, 1
 	li $v1, 0
-	li $t7, ' ' # check for space
-	li $t8, '\n'
+	li $t2, 32 # space character ascii code
+	li $t3, 10 # newline character ascii code
 		
-loop:	# v0 = words, v1 = characters, 
-	lb $t6, ($t5)	#load character
+loop:
+	lb $t1, ($t0)	#load character
 	
-	bne $t6, $t7, continue
+	bne $t1, $t2, continue
 	addi $v0, $v0, 1
 	
 continue:
-	beq $t6, $zero, breakLoop	# check for '\0' character (null-terminator)
-	beq $t6, $t8, breakLoop	# check for '\n' character (newline)
+	beq $t1, $zero, breakLoop	# check for '\0' character (null-terminator)
+	beq $t1, $t3, breakLoop	# check for '\n' character (newline)
 	
 	addi $v1, $v1, 1	#increment numbe of characters
-	addi $t5, $t5, 1	#increment character address
+	addi $t0, $t0, 1	#increment character address
 	
 	j loop
 	
 breakLoop:
 	beq $v1, $zero, exit
 	jr $ra
-	
-clearString:
-	
 
 print:	
 	li $v0, 4
@@ -72,7 +69,11 @@ print:
 	
 	li $v0, 4
 	la $a0, charOutput
-	syscall 
+	syscall
+	
+	li $v0, 11
+	li $a0, 10
+	syscall
 	
 	j main
 
